65a681eb9f5d821ec0d9d87cb7cbcdfa436c4e69
Zach Pratt <zppratt@gmail.com>
2022-11-13 19:19:44 -0500

    cors

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
 	gin.SetMode(gin.DebugMode)
 	r.POST("/login", handleLogin)
 	r.Run() // listen and serve on
